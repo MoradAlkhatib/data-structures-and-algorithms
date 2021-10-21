64610828123e602dc8afd016cefb058e191a001c
moradalkhatib <muradalkhateb@icloud.com>
2021-10-21 14:51:59 +0300

    implementation Queue

--- python/challenges/stack_and_queue/stack.py
+++ python/challenges/stack_and_queue/stack.py
@@ -2,7 +2,8 @@ class Node:
     def __init__(self,item,next=None) :
          self.item=item
          self.next=next
-
+# A class to represent a Stack
+# The stack, last stores the front node
 class Stack:
     def __init__(self):
         self.top=None
@@ -12,13 +13,13 @@ class Stack:
             return True
         else:
             return False
-
+    # Method to add an item to the stack
     def push(self,item):
         node=Node(item)
         if self.top:
            node.next=self.top
         self.top=node
-
+    # Method to remove an item from stack
     def pop(self):
         if not self.top:
             return None
@@ -27,11 +28,14 @@ class Stack:
             self.top = temp.next
             temp.next = None
             return temp.item
+
     def peek(self):
         if self.isempty():
             return None
         else:
             return self.top.item
+
+
 
     # def to_string(self):
     #     str = ""
@@ -56,4 +60,4 @@ if __name__=='__main__':
     stack.push(5)
     stack.push(5)
     stack.push(5)
-    stack.to_string
+    print(stack.top.item)
