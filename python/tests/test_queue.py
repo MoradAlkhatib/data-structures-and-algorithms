2e7b4f7bebaeaa812a232ae5aa7341298728d3c0
moradalkhatib <muradalkhateb@icloud.com>
2021-10-25 21:04:14 +0300

    m

--- python/tests/test_queue.py
+++ python/tests/test_queue.py
@@ -2,38 +2,38 @@ from challenges.stack_and_queue.queue import (Queue,Node)
 # Can successfully enqueue into a queue
 def test_enqueue():
     q=Queue()
-    q.EnQueue(5)
+    q.enqueue(5)
     assert q.front.item==5
 # Can successfully enqueue multiple values into a queue
 def test_multiple_enqueue():
     q=Queue()
-    q.EnQueue(5)
-    q.EnQueue(6)
-    q.EnQueue(7)
+    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
     assert q.front.item==5
     assert q.rear.item==7
 # Can successfully dequeue out of a queue the expected value
 def test_dequeue():
     q=Queue()
-    q.EnQueue(5)
-    q.EnQueue(6)
-    q.EnQueue(7)
-    q.DeQueue()
+    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
+    q.dequeue()
     assert q.front.item==6
 # Can successfully peek into a queue, seeing the expected value
 def test_peek():
     q=Queue()
-    q.EnQueue(5)
-    q.EnQueue(6)
-    q.EnQueue(7)
+    q.enqueue(5)
+    q.enqueue(6)
+    q.enqueue(7)
     assert q.peek()==5
 # Can successfully empty a queue after multiple dequeues
 def test_is_empty():
     q=Queue()
-    q.EnQueue(5)
-    q.EnQueue(6)
-    q.DeQueue()
-    q.DeQueue()
+    q.enqueue(5)
+    q.enqueue(6)
+    q.dequeue()
+    q.dequeue()
     assert q.isEmpty()==True
 # Can successfully instantiate an empty queue
 def test_empty():
@@ -43,4 +43,4 @@ def test_empty():
 def test_dequeue_or_peek_on_empty():
     q=Queue()
     assert q.peek()=="Queue is empty"
-    assert q.DeQueue()=="Queue is empty"
+    assert q.dequeue()=="Queue is empty"
